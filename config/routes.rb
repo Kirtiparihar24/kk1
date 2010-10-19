f03d90b771003eb96042cd6cfd42ef546a34893b
kirti <pariharkirti24@gmail.com>
2010-10-19 12:02:09 +0530

    Done with static pages

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Five::Application.routes.draw do
   get "pages/home"
 
   get "pages/contact"
+  
+  get "pages/about"
+
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
